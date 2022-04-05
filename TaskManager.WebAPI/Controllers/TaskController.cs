4a82ae6a26cad319dc0e2f1a970d35849d42e524
Starinin Andrey <starininandrey@gmail.com>
2022-04-05 16:32:27 +0300

    Add controllers

---
+++ TaskManager.WebAPI/Controllers/TaskController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/v0/task")]
+public class TaskController : ControllerBase
+{
+    [HttpGet("{id:int}")]
+    public JsonResult GetTask(int id)
+    {
+        return new JsonResult(new {result = true, _id = id});
+    }
+    
+    [HttpPost("create")]
+    public JsonResult CreateTask()
+    {
+        return new JsonResult(new {result = true});
+    }
+    
+    [HttpPut("update/{id:int}")]
+    public JsonResult UpdateTask(int id)
+    {
+        return new JsonResult(new {result = true, _id = id});
+    }
+    
+    [HttpDelete("delete/{id:int}")]
+    public JsonResult DeleteTask(int id)
+    {
+        return new JsonResult(new {result = true, _id = id});
+    }
+}
---
+++ TaskManager.WebAPI/Controllers/TasksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/v0/tasks")]
+public class TasksController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetAllTasks()
+    {
+        return new JsonResult(new {result = true});
+    }
+}
