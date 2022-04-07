aaa8b39c451f4e897386d48fffcfc4163475d964
Starinin Andrey <starininandrey@gmail.com>
2022-04-07 16:03:16 +0300

    Add DataBase config

---
+++ TaskManager.DB/DB_Config/DataBaseConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Text.Json;
+
+namespace TaskManager.DB.DB_Config;
+
+public class DataBaseConfig
+{
+    public string Server { get; set; }
+    public string Database { get; set; }
+    public string Uid { get; set; }
+    public string Pwd { get; set; }
+
+    public override string ToString()
+    {
+        //Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+        return $"{nameof(Server)}={Server};{nameof(Database)}={Database};{nameof(Uid)}={Uid};{nameof(Pwd)}={Pwd}";
+    }
+
+    public static DataBaseConfig CreateFromJson(string json)
+    {
+        try
+        {
+            var config = JsonSerializer.Deserialize<DataBaseConfig>(json);
+            if (config is null) throw new NoNullAllowedException();
+            return config;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
