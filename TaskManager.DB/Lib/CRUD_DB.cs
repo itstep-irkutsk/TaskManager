8f1f1e079f48636e0f3227c80504da35b08081e6
Starinin Andrey <starininandrey@gmail.com>
2022-04-11 16:27:57 +0300

    Add methods to CRUD_DB

--- TaskManager.DB/Lib/CRUD_DB.cs
+++ TaskManager.DB/Lib/CRUD_DB.cs
@@ -1,6 +1,29 @@
-﻿namespace TaskManager.DB.Lib;
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using TaskManager.DB.DB_Config;
+using TaskManager.DB.Models;
+
+namespace TaskManager.DB.Lib;
 
 public class CRUD_DB
 {
-    
+    private MySqlConnection _db;
+
+    public CRUD_DB(string connectionConfig)
+    {
+        var configDb = DataBaseConfig.CreateFromJson(connectionConfig);
+        _db = new MySqlConnection(configDb.ToString());
+    }
+
+    public async Task<IEnumerable<Stage>> GetAllStagesAsync()
+    {
+        await _db.OpenAsync();
+
+        var sql = "SELECT id, description, date_of_creation, task_id FROM tbl_stages";
+        var stages = await _db.QueryAsync<Stage>(sql);
+        
+        await _db.CloseAsync();
+
+        return stages;
+    }
 }
